85f4438b8ce8d5e736eb496a976d357da07e1a8f
“tahame01” <ahamedtangir@yahoo.com>
2022-03-24 22:54:55 +0000

    jj

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ym^7h8u^5_l2+lfq1572ze8nrwds702^l04w04)+527oj294(j'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = ["209.97.141.245"]
 
