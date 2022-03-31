a37f734d1c74a882e0c54dc727798bb31efb3254
Tangir Ahamed <tangirahamed@Tangirs-iMac.local>
2022-03-31 09:55:30 +0100

    changed ip

--- project_settings/settings.py
+++ project_settings/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ["209.97.141.245"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
